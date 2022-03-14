7135aeae4466bbf88dec57f14f1392335a666af1
Kyle D'Amato <kyledamato4@gmail.com>
2022-03-14 13:17:37 -0400

    created JS code for username window prompt, question prompt and correct answer selection

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your Code Here
+//Window Prompt to enter and save name of player
+let userName = window.prompt('Please enter your name:')
+
+//window prompt for questions and answer selection-correct answer JS coding at bottom
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        console.log('Correct')
+    }
+}
